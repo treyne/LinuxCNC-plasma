e384965a9b0d1fc828cd75ec5ea6e5f5376b247b
treyne <treyne@mail.ru>
2023-08-08 16:14:40 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo halcompile --install hood.comp
+
+sudo cp X-axis-auto-scale.py /usr/local/bin/
+sudo chmod +x /usr/local/bin/X-axis-auto-scale.py
+
+
+sudo cp TUBE_MOVE.py /usr/local/bin/
+sudo chmod +x /usr/local/bin/TUBE_MOVE.py
+
+
+sudo cp TubeWAIT.py /usr/local/bin/
+sudo chmod +x /usr/local/bin/TubeWAIT.py
